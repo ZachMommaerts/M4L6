44cb5440860ac2f3cfadd4bfc2cd009bf2689fe0
ZachMommaerts <zmommaerts@gmail.com>
2023-07-18 10:51:47 -0500

    Initial Commit

---
+++ M4L6/M4L6App.swift
@@ -0,0 +1,17 @@
+//
+//  M4L6App.swift
+//  M4L6
+//
+//  Created by Zach Mommaerts on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct M4L6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
